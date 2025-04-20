e6d7c087cf611ac30a96282b3e62609773226789
Derek Zhu <daz005@ucsd.edu>
2025-04-19 21:38:12 -0700

    fix bugs

--- module10-solution/src/common/menu.service.js
+++ module10-solution/src/common/menu.service.js
@@ -11,6 +11,7 @@ function MenuService($http, ApiPath) {
 
   service.getCategories = function () {
     return $http.get(ApiPath + '/categories.json').then(function (response) {
+      //console.log("getCategories response.data=" + response.data);
       return response.data;
     });
   };
@@ -18,12 +19,34 @@ function MenuService($http, ApiPath) {
 
   service.getMenuItems = function (category) {
     return $http.get(ApiPath + '/menu_items/' + category + '.json').then(function (response) {
+
+      //console.log("getMenuItems response.data=" + response.data);
       return response.data;
     });
   };
 
-}
 
+  service.getMenuItem = function (shortName) {
+    //console.log("getMenuItem shortName=" + shortName);
 
+    var category = shortName.charAt(0).toUpperCase();
+    var index = parseInt(shortName.slice(1)) - 1;
+    var url = "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/" +
+              category + "/menu_items/" + index + ".json";
+    return $http.get(url).then(function (response) {
+      // console.log("getMenuItem response.data.getCategories=" + response.data.getCategories);
+      // console.log(response.data);
+      // console.log(url);
+      response.data.categoryShortName = category;
+      return response.data;
+    });
+  };
+
+};
 
 })();
+
+
+
+
+
--- module10-solution/src/public/myinfo/myinfo.controller.js
+++ module10-solution/src/public/myinfo/myinfo.controller.js
@@ -13,7 +13,12 @@
   
       if (myInfoCtrl.user && myInfoCtrl.user.favDish) {
         MenuService.getMenuItem(myInfoCtrl.user.favDish).then(function (data) {
+
           myInfoCtrl.menuItem = data;
+
+          //console.log("myInfoCtrl.menuItem.Name=" + myInfoCtrl.menuItem.Name);
+          //console.log("myInfoCtrl.menuItem.short_name=" + myInfoCtrl.menuItem.short_name);
+          //console.log("myInfoCtrl.menuItem.categoryShortName=" + myInfoCtrl.menuItem.categoryShortName);
         });
       }
     }
